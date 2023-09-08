7b1fe321f5ac5157f26fb370affac71c4b2b76e1
unknown <insight.outside@disroot.org>
2023-09-08 21:38:12 +0300

    basic layout

--- index.ts
+++ index.ts
@@ -5,23 +5,17 @@ import {
   serveStatic,
 } from "https://deno.land/x/hono@v3.5.5/middleware.ts";
 
+import search from "./search.tsx";
 const app = new Hono();
-
 app.use("*", logger());
 app.use("/static/*", serveStatic({ root: "./" }));
 
 app.get("/", serveStatic({ path: "./index.html" }));
 app.get("/content", serveStatic({ path: "./static/content.html" }));
 app.get("/left", serveStatic({ path: "./static/leftbar.html" }));
-app.post("/right", serveStatic({ path: "./static/rightbar.html" }));
-app.post("/search", (c) => {
-  if (c.req.header("HX-Request")) {
-    const input = c.req.queries("q");
-    return c.text(input?.values(), 300);
-  }
-  return c.text("not found", 404);
-  // logger(input);
-});
+// app.post("/right", serveStatic({ path: "./static/rightbar.html" }));
+// serveStatic({ path: "./static/rightbar.html" });
+app.route("/search", search);
 app.showRoutes();
 app.get("*", serveStatic({ path: "./static/404.html" }));
 Deno.serve(app.fetch);
