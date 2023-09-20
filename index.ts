d9c74dc7a719dc6e07890edfed3fe76337e1449c
unknown <insight.outside@disroot.org>
2023-09-20 19:38:30 +0300

    feat: basic rednode cors

--- index.ts
+++ index.ts
@@ -1,14 +1,70 @@
-import { Hono } from "https://deno.land/x/hono@v3.5.5/mod.ts";
+import { Hono, HonoRequest } from "https://deno.land/x/hono@v3.5.5/mod.ts";
 import { html } from "https://deno.land/x/hono@v3.5.5/helper.ts";
 import {
+  cors,
   logger,
   serveStatic,
 } from "https://deno.land/x/hono@v3.5.5/middleware.ts";
 
-import search from "./src/search.tsx";
+import search from "./search.tsx";
 const app = new Hono();
-app.use("*", logger());
+
 app.use("/static/*", serveStatic({ root: "./" }));
+app.use("/css/*", serveStatic({ root: "./" }));
+app.use("/red/*",cors({
+  origin:"localhost:1880",
+  allowMethods: ['POST', 'GET', 'PUT', 'DELETE'],
+  maxAge: 600,
+}));
+app.get("/red/temp", async (c) => {
+  const res = await fetch("http://127.0.0.1:1880/sensor/temp",{
+    method: "GET",
+    // body: `{
+    //   msg: {
+    //     payload: {
+    //       target: "test"
+    //     },
+    //   },
+    // }` as BodyInit
+    
+  })
+  return c.html(await res.text(),res.status);
+  // return c.json({msg:{
+  //   payload: 1
+  // }});
+});
+
+app.on("POST","/red/state", async (c) => {
+  let state = await c.req.json()
+  if (state.time !== 0) {
+    
+  }
+  console.log(state);
+  // let res = await c.req.body?.getReader().read().then((status)=>{
+
+    // console.log(text);
+  
+  return c.json({status:"accepted", name: state.name},202)
+});
+app.post("/red/status/:room", async (c) => {
+  // TODO: либо сделать объект { room: target } либо делать это в nodered
+  const room = c.req.param("room");
+  console.log(room)
+
+  const body = JSON.stringify({
+
+    target: `${room}`,
+    msg : "test"
+    })
+  const res = await fetch("http://127.0.0.1:1880/switch",{
+    method: "POST",
+    body:  body
+  })
+  return c.html(await res.text(),res.status);
+  // return c.json({msg:{
+  //   payload: 1
+  // }});
+});
 
 app.get("/", serveStatic({ path: "./index.html" }));
 app.get("/content", serveStatic({ path: "./static/content.html" }));
@@ -16,6 +72,14 @@ app.get("/left", serveStatic({ path: "./static/leftbar.html" }));
 // app.post("/right", serveStatic({ path: "./static/rightbar.html" }));
 // serveStatic({ path: "./static/rightbar.html" });
 app.route("/search", search);
+
+app.use("*", logger());
 app.showRoutes();
-app.get("*", serveStatic({ path: "./static/404.html" }));
+app.onError((e,c)=>{
+  
+  console.error(e); 
+  // app.get("*", serveStatic({ path: "./static/404.html" }));
+  return c.html(`<div class=" w3-card-4 w3-red w3-opacity"><h1>${e}</h1></div>`,400);
+});
+
 Deno.serve(app.fetch);
---
+++ search.tsx
@@ -0,0 +1,185 @@
+/** @jsx jsx */
+/** @jsxFrag Fragment */
+
+import { MeiliSearch } from "https://esm.sh/meilisearch";
+
+import {
+  html,
+  jsx,
+  memo,
+} from "https://deno.land/x/hono@v3.5.5/middleware.ts";
+import { Hono } from "https://deno.land/x/hono@v3.5.5/mod.ts";
+import { MeiliSearchError } from "https://esm.sh/meilisearch@0.34.2";
+import { MeiliSearchApiError } from "https://esm.sh/meilisearch@0.34.2";
+import { Context } from "https://deno.land/x/hono@v3.5.5/mod.ts";
+
+
+const search = new Hono();
+
+let index: any;
+let search_query = "" ;
+let search_results: any ;
+let page = 1 ;
+
+try {
+  const master_key = Deno.env.get("MEILI_MASTER_KEY");
+  const search_client = new MeiliSearch({
+  host: "http://127.0.0.1:7700",
+  apiKey: master_key,
+  });
+
+  // TODO: это временное решение подгружать данные из файла
+  const decoder = new TextDecoder("utf-8");
+  const fd = await Deno.readFile("./static/test_data.json");
+  const str = decoder.decode(fd);
+  const db = JSON.parse(str);
+  
+  if (!search_client) {
+    // search.fire();
+    throw new MeiliSearchError("Can't create search client",503)
+}
+
+  const health = await search_client.health();
+  if (!health && health.status !== "available") {
+    throw new MeiliSearchApiError("MeiliSearch not available right now!",100);
+  }
+  // if (search_client.health().status === "") {}
+  // An index is where the documents are stored.
+
+  // const task = JSON.parse(response);
+    index = await search_client.index("rent");
+    const response = await index.addDocuments(db, { primaryKey: "uid" });
+    const status = await search_client.getTask(response.taskUid);
+
+
+
+  // const e = error;
+  // search.all("*",async (_,next)=> {await next()})
+  search.use("*", async (c,next) => {
+      if (c.req.header("HX-Request")) {
+        search_query = c.req.query("q") as string;
+        page = Number(c.req.query("page"));
+        if (isNaN(page)) {
+          page = 1;
+        }
+      }
+    if (!index) {
+      throw new MeiliSearchApiError();
+
+    }
+      // BUG: might be recursive page 1 
+    search_results = await index.search(search_query,{hitsPerPage:80, page : page});
+  // INFO: result format
+  //   {
+  //    hits: [
+  //   ],
+  //    query
+  //    processingTimeMs
+  //    hitsPerPage
+  //    page
+  //    totalPages
+  //    totalHits 
+  //   }
+      let Result = ()=>html``;
+      let Header = memo(()=>html``);
+      if (search_results.totalHits === 0 ) {
+        Header = memo(() => {
+          return (html`
+            <h3> nothing found</h3>
+          `);
+        });
+      } 
+      else {
+        if (page===1) {
+          Header = memo(() => {
+            return (html`
+                    <div class="w3-container">
+                      <h1 class="w3-left-align">${search_results.query}</h1> 
+                      <p class="w3-right-align">наидено ${search_results.totalHits} шт. за ${search_results.processingTimeMs} мс.</p>
+                    </div>
+            `)
+          });
+        }
+
+        // if (search_results.totalPages>1 && page<=search_results.totalPages) {
+          // result.hits;
+          Result = () => {
+            const items = [];
+            for (const key in search_results.hits) {
+                const { uid,id,название:name} = search_results.hits[key];
+                // INFO: temp rent item schema 
+                //  {
+                //    uid,
+                //    id,
+                //    название,
+                //  }
+                //
+                if (+key === search_results.hitsPerPage-1) {
+                  items.push(
+                    html`
+                      <li class="w3-hover-yellow w3-card-4 w3-panel"  
+                      onclick="/locate/${uid}"  
+                      hx-get="/search?q=${search_query}&page=${search_results.page+1} " 
+                      hx-trigger="intersect once"
+                      hx-swap="afterend">
+                      <header><h3>${name}</h3></header>
+                      <p class="w3-right">${id}</p>
+                    </li>`);
+                }
+                else {
+                  items.push(
+                    html`<li class="w3-hover-yellow w3-card-4 w3-panel"  
+                      hx-get="/locate/${uid} ">
+                      <header><h3>${name}</h3></header>
+                      <p class="w3-right">${id}</p>
+                    </li>`)
+                }
+              }
+              console.error(search_results)
+            return (html`
+                ${items}
+                `);
+          };
+      }
+      return c.html(
+      <div>
+        <Header/>
+        <ul class="w3-ul w3-panel">
+          <Result />
+        </ul>
+      </div>
+      );
+    
+  });
+
+}
+catch(e: any){
+  // INFO: meilisearch error format some errors have name in it
+  //   {
+  //   "message": "Index `movies` not found.",
+  //   "code": "index_not_found",
+  //   "type": "invalid_request",
+  //   "link": "https://docs.meilisearch.com/errors#index_not_found",
+  //   "httpStatus: 404
+  //  }
+  search.onError((e: any,c: Context)=>{
+    console.error(e);
+    switch (e.constructor) {
+      case MeiliSearchError:
+        return c.json(e,503);
+      case MeiliSearchApiError:
+        return c.json(e,e.httpStatus);
+      default:
+        return c.json(e,500);
+    }
+  })
+}
+finally {
+search.onError((e: any,c: Context)=>{
+      console.error(e);
+      return c.text(e.message,404)
+});
+}
+export default search;
+
+
